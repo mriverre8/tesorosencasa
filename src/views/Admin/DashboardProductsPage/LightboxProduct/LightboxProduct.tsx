10d994d1f840c94ec5d55250df41c96a6d1dd3dd
mriverre8 <rivemark00@gmail.com>
2025-07-10 10:22:25 +0200

    added share btn on product

--- src/views/Admin/DashboardProductsPage/LightboxProduct/LightboxProduct.tsx
+++ src/views/Admin/DashboardProductsPage/LightboxProduct/LightboxProduct.tsx
@@ -83,19 +83,12 @@ const LightboxProduct = ({ tesoros, setTesoros }: Props) => {
     const url = `https://tesorosencasa.vercel.app/tesoro/${lightboxProduct.product.id}`;
     navigator.clipboard.writeText(url).then(() => {
       setCopiado(true);
-      setTimeout(() => setCopiado(false), 4000);
+      setTimeout(() => setCopiado(false), 10000);
     });
   };
 
   return (
     <Lightbox isOpen={lightboxProduct.isOpen}>
-      {copiado && (
-        <div className="w-full text-center">
-          <span className="text-sm text-secondary ">
-            {translate('LINK_COPIED')}
-          </span>
-        </div>
-      )}
       <div className="flex">
         <h1 className="flex-[8] text-2xl font-bold">
           {lightboxProduct.product.name}
@@ -106,138 +99,166 @@ const LightboxProduct = ({ tesoros, setTesoros }: Props) => {
           </button>
         </div>
       </div>
-      <div className="flex flex-col gap-3 h-[400px] overflow-y-auto mt-3">
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col">
-            <p className="text-xs text-gray-400">
-              {translate('TREASSAURE_CONDITION')}
-            </p>
-            <p className="text-sm">{lightboxProduct.product.condition}</p>
+
+      <div className=" h-[400px] overflow-y-auto ">
+        {copiado && (
+          <div className="w-full">
+            <span className="text-xs text-secondary ">
+              {translate('LINK_COPIED')}
+            </span>
           </div>
-        </div>
-        <h2 className="font-semibold">{translate('TREASAURE_BACKGROUND')}</h2>
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_ORIGIN')}
-            </p>
-            <p className="text-sm">
-              {lightboxProduct.product.origin
-                ? lightboxProduct.product.origin
-                : translate('UNKNOWN')}
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_BRAND')}
-            </p>
-            <p className="text-sm ">
-              {lightboxProduct.product.brand
-                ? lightboxProduct.product.brand
-                : translate('UNKNOWN')}
-            </p>
-          </div>
-        </div>
-        <h2 className="font-semibold">{translate('TREASAURE_FABRICATION')}</h2>
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_MATERIAL')}
-            </p>
-            <div className="text-sm">
-              {lightboxProduct.product.material ? (
-                <p>
-                  {Object.values(lightboxProduct.product.material).join(' - ')}
-                </p>
-              ) : (
-                translate('UNKNOWN')
-              )}
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_CATEGORY')}
-            </p>
-            <p className="text-sm">
-              {lightboxProduct.product.category
-                ? lightboxProduct.product.category
-                : translate('UNKNOWN')}
-            </p>
-          </div>
-        </div>
-        {(lightboxProduct.product.large ||
-          lightboxProduct.product.width ||
-          lightboxProduct.product.height ||
-          lightboxProduct.product.diameter) && (
-          <>
-            <h2 className="font-semibold">
-              {translate('TREASAURE_DIMENSIONS')}
-            </h2>
-            <div className="flex flex-row gap-5">
-              {lightboxProduct.product.large && (
-                <div className="flex flex-col justify-center items-center text-center">
-                  <p className="text-xs text-gray-400">{translate('LARGE')}</p>
-                  <p className="text-sm">{lightboxProduct.product.large} cm</p>
-                </div>
-              )}
-              {lightboxProduct.product.width && (
-                <div className="flex flex-col justify-center items-center text-center">
-                  <p className="text-xs text-gray-400">{translate('WIDTH')}</p>
-                  <p className="text-sm">{lightboxProduct.product.width} cm</p>
-                </div>
-              )}
-              {lightboxProduct.product.height && (
-                <div className="flex flex-col justify-center items-center text-center">
-                  <p className="text-xs text-gray-400">{translate('HEIGHT')}</p>
-                  <p className="text-sm">{lightboxProduct.product.height} cm</p>
-                </div>
-              )}
-              {lightboxProduct.product.diameter && (
-                <div className="flex flex-col justify-center items-center text-center">
-                  <p className="text-xs text-gray-400">
-                    {translate('DIAMETER')}
-                  </p>
-                  <p className="text-sm">
-                    {lightboxProduct.product.diameter} cm
-                  </p>
-                </div>
-              )}
-            </div>
-          </>
         )}
-        <h2 className="font-semibold">{translate('TREASAURE_STOCK')}</h2>
-        <div className="flex flex-row gap-5">
-          <div className="flex flex-col justify-center items-center text-center">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_UNITS')}
-            </p>
-            <p className="text-sm">{lightboxProduct.product.units}</p>
+        <div className="mt-3 flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
+              <p className="text-xs text-gray-400">
+                {translate('TREASSAURE_CONDITION')}
+              </p>
+              <p className="text-sm">{lightboxProduct.product.condition}</p>
+            </div>
           </div>
-          <div className="flex flex-col justify-center items-center text-center">
-            <p className="text-xs text-gray-400">
-              {translate('TREASAURE_PRICE')}{' '}
-              {translate('TREASAURE_PRICE_X_UNITS')}
-            </p>
-            <p className="text-sm">{lightboxProduct.product.price} €</p>
+          <h2 className="font-semibold">{translate('TREASAURE_BACKGROUND')}</h2>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_ORIGIN')}
+              </p>
+              <p className="text-sm">
+                {lightboxProduct.product.origin
+                  ? lightboxProduct.product.origin
+                  : translate('UNKNOWN')}
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_BRAND')}
+              </p>
+              <p className="text-sm ">
+                {lightboxProduct.product.brand
+                  ? lightboxProduct.product.brand
+                  : translate('UNKNOWN')}
+              </p>
+            </div>
           </div>
-        </div>
-        <h2 className="font-semibold">{translate('TREASAURE_MULTIMEDIA_1')}</h2>
-        <div className="pb-2">
-          <div className="flex gap-2 w-full overflow-x-auto">
-            {lightboxProduct.product.images.map((image, index) => (
-              <div
-                key={index}
-                className="inline-block w-24 h-24 relative overflow-hidden rounded-lg bg-gray-200 shrink-0"
-              >
-                <CldImage
-                  width="960"
-                  height="600"
-                  src={image}
-                  sizes="100vw"
-                  alt="Description of my image"
-                />
+          <h2 className="font-semibold">
+            {translate('TREASAURE_FABRICATION')}
+          </h2>
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_MATERIAL')}
+              </p>
+              <div className="text-sm">
+                {lightboxProduct.product.material ? (
+                  <p>
+                    {Object.values(lightboxProduct.product.material).join(
+                      ' - '
+                    )}
+                  </p>
+                ) : (
+                  translate('UNKNOWN')
+                )}
               </div>
-            ))}
+            </div>
+            <div className="flex flex-col">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_CATEGORY')}
+              </p>
+              <p className="text-sm">
+                {lightboxProduct.product.category
+                  ? lightboxProduct.product.category
+                  : translate('UNKNOWN')}
+              </p>
+            </div>
+          </div>
+          {(lightboxProduct.product.large ||
+            lightboxProduct.product.width ||
+            lightboxProduct.product.height ||
+            lightboxProduct.product.diameter) && (
+            <>
+              <h2 className="font-semibold">
+                {translate('TREASAURE_DIMENSIONS')}
+              </h2>
+              <div className="flex flex-row gap-5">
+                {lightboxProduct.product.large && (
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <p className="text-xs text-gray-400">
+                      {translate('LARGE')}
+                    </p>
+                    <p className="text-sm">
+                      {lightboxProduct.product.large} cm
+                    </p>
+                  </div>
+                )}
+                {lightboxProduct.product.width && (
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <p className="text-xs text-gray-400">
+                      {translate('WIDTH')}
+                    </p>
+                    <p className="text-sm">
+                      {lightboxProduct.product.width} cm
+                    </p>
+                  </div>
+                )}
+                {lightboxProduct.product.height && (
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <p className="text-xs text-gray-400">
+                      {translate('HEIGHT')}
+                    </p>
+                    <p className="text-sm">
+                      {lightboxProduct.product.height} cm
+                    </p>
+                  </div>
+                )}
+                {lightboxProduct.product.diameter && (
+                  <div className="flex flex-col justify-center items-center text-center">
+                    <p className="text-xs text-gray-400">
+                      {translate('DIAMETER')}
+                    </p>
+                    <p className="text-sm">
+                      {lightboxProduct.product.diameter} cm
+                    </p>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+          <h2 className="font-semibold">{translate('TREASAURE_STOCK')}</h2>
+          <div className="flex flex-row gap-5">
+            <div className="flex flex-col justify-center items-center text-center">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_UNITS')}
+              </p>
+              <p className="text-sm">{lightboxProduct.product.units}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center">
+              <p className="text-xs text-gray-400">
+                {translate('TREASAURE_PRICE')}{' '}
+                {translate('TREASAURE_PRICE_X_UNITS')}
+              </p>
+              <p className="text-sm">{lightboxProduct.product.price} €</p>
+            </div>
+          </div>
+          <h2 className="font-semibold">
+            {translate('TREASAURE_MULTIMEDIA_1')}
+          </h2>
+          <div className="pb-2">
+            <div className="flex gap-2 w-full overflow-x-auto">
+              {lightboxProduct.product.images.map((image, index) => (
+                <div
+                  key={index}
+                  className="inline-block w-24 h-24 relative overflow-hidden rounded-lg bg-gray-200 shrink-0"
+                >
+                  <CldImage
+                    width="960"
+                    height="600"
+                    src={image}
+                    sizes="100vw"
+                    alt="Description of my image"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
