5c264978a75d85aecfeb19d99e81093271824355
mriverre8 <rivemark00@gmail.com>
2025-09-23 11:01:15 +0200

    avoiding re-fetch in product

--- src/app/tesoro/[id]/page.tsx
+++ src/app/tesoro/[id]/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Layout from '@/components/Layout';
-import { getProductById } from '@/actions/getProductById';
 import ProductPage from '@/views/Product/ProductPage';
 import Topbar from '@/components/Topbar';
 import Footer from '@/components/Footer';
 import { createClient } from '@/supabase/server';
-import { getUserById } from '@/actions/getUserById';
 
 export default async function Product({
   params,
@@ -13,23 +11,19 @@ export default async function Product({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const tesoroData = await getProductById(id);
 
-  // Getting user data to see if user is loged in and has admin role
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  const userData = user ? await getUserById(user.id) : null;
-
   return (
     <div className="flex flex-col justify-between min-h-screen bg-background">
-      <Topbar isAdminLogged={userData?.role === 'ADMIN'} />
+      <Topbar isAdminLogged={user != null} />
       <main>
         <div className="mt-[90px]">
           <Layout>
-            <ProductPage productData={tesoroData} />
+            <ProductPage id={id} />
           </Layout>
         </div>
       </main>
--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ButtonSecondary = ({ buttonText, buttonAction }: Props) => {
   return (
     <button
-      className="py-0.5 px-4 hover:text-primary transition duration-300 underline underline-offset-2 whitespace-nowrap"
+      className="py-0.5 hover:text-primary transition duration-300 underline underline-offset-2 whitespace-nowrap"
       onClick={buttonAction}
     >
       {buttonText}
