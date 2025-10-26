dc3c046ed8da41fe741b63f54da6fe1b8be0ae46
mriverre8 <rivemark00@gmail.com>
2025-10-26 16:01:54 +0100

    Common loader

--- src/components/common/StreamCardSkeleton.tsx
+++ src/components/common/StreamCardSkeleton.tsx
@@ -3,14 +3,21 @@ import React from 'react';
 const StreamCardSkeleton = () => {
   return (
     <div className="flex bg-white p-2 rounded-md gap-2 items-center justify-between shadow-md animate-pulse">
-      <div className="flex gap-2 flex-1">
-        <div className="p-2 bg-gray-300 rounded-md w-16 h-10" />
-        <div className="flex flex-col flex-1 justify-center gap-2">
-          <div className="h-4 bg-gray-300 rounded w-32" />
-          <div className="h-3 bg-gray-300 rounded w-24" />
+      <div className="flex gap-2 w-full">
+        <div className="flex p-2 bg-background rounded-md justify-center items-center w-[80px] h-[45px]">
+          <div className="h-4 w-10 bg-gray-200 rounded" />
+        </div>
+
+        <div className="flex flex-col justify-center flex-1 gap-2">
+          <div className="h-3 w-24 bg-gray-200 rounded" />
+          <div className="h-2 w-32 bg-gray-200 rounded" />
         </div>
       </div>
-      <div className="w-8 h-8 bg-gray-300 rounded-full" />
+
+      <div className="flex items-center gap-1">
+        <div className="h-3 w-3 bg-gray-200 rounded-full" />
+        <div className="h-2 w-12 bg-gray-200 rounded" />
+      </div>
     </div>
   );
 };
